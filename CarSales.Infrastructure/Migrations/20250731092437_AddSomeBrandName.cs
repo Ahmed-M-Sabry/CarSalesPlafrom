8f0e61f60eace49712721f52d2d72d312277fd72
Ahmed Sabry <a7med.mohamed.sabri@gmail.com>
2025-07-31 12:30:50 +0300

    AddFuelType , Brand and TransmissionType data in migration

---
+++ CarSales.Infrastructure/Migrations/20250731092437_AddSomeBrandName.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarSales.Infrastructure.Migrations
+{
+    public partial class AddSomeBrandName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Brands",
+                columns: new[] { "Id", "Name", "IsDeleted" },
+                values: new object[,]
+                {
+                    { 1, "Toyota", false },
+                    { 2, "Hyundai", false },
+                    { 3, "Nissan", false },
+                    { 4, "Honda", false },
+                    { 5, "Kia", false },
+                    { 6, "Ford", false },
+                    { 7, "Chevrolet", false },
+                    { 8, "BMW", false },
+                    { 9, "Mercedes-Benz", false },
+                    { 10, "Volkswagen", false },
+                    { 11, "Mitsubishi", false },
+                    { 12, "Peugeot", false },
+                    { 13, "Renault", false },
+                    { 14, "Suzuki", false },
+                    { 15, "Fiat", false }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Brands",
+                keyColumn: "Id",
+                keyValues: new object[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 });
+        }
+    }
+}
---
+++ CarSales.Infrastructure/Migrations/20250731092912_AddFuelTypeAndTransmissionTypeData.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarSales.Infrastructure.Migrations
+{
+    public partial class AddFuelTypeAndTransmissionTypeData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Seed data for FuelTypes
+            migrationBuilder.InsertData(
+                table: "FuelTypes",
+                columns: new[] { "Id", "Name", "IsDeleted" },
+                values: new object[,]
+                {
+                    { 1, "Petrol", false },
+                    { 2, "Diesel", false },
+                    { 3, "Hybrid", false },
+                    { 4, "Electric", false }
+                });
+
+            // Seed data for TransmissionTypes
+            migrationBuilder.InsertData(
+                table: "TransmissionTypes",
+                columns: new[] { "Id", "Name", "IsDeleted" },
+                values: new object[,]
+                {
+                    { 1, "Manual", false },
+                    { 2, "Automatic", false },
+                    { 3, "CVT", false },
+                    { 4, "Dual-Clutch", false }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Remove FuelTypes data
+            migrationBuilder.DeleteData(
+                table: "FuelTypes",
+                keyColumn: "Id",
+                keyValues: new object[] { 1, 2, 3, 4 });
+
+            // Remove TransmissionTypes data
+            migrationBuilder.DeleteData(
+                table: "TransmissionTypes",
+                keyColumn: "Id",
+                keyValues: new object[] { 1, 2, 3, 4 });
+        }
+    }
+}
